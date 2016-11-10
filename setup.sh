93ad4fc8abff5320998e13c26e1848573a5fb19c
Andrew Hundt <ATHundt@gmail.com>
2016-11-10 17:40:19 -0500

    commented setup line

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 
 export HADOOP_CLASSPATH=$JAVA_HOME/lib/tools.jar
+export PATH=$PATH:/home/parallel/hadoop/bin/
+export JAVA_HOME=/usr/lib/jvm/java-7-oracle/
 
-hadoop jar hadoop/share/hadoop/tools/lib/hadoop-streaming-2.7.3.jar -mapper ./fof.mapper.pl -reducer fof.reducer.pl -input simple.input -output simple.output
+#hadoop jar hadoop/share/hadoop/tools/lib/hadoop-streaming-2.7.3.jar -mapper ./fof.mapper.pl -reducer fof.reducer.pl -input simple.input -output simple.output
 
 
 hadoop com.sun.tools.javac.Main FoF.java
