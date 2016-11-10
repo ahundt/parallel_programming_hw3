ad4d160fda63a6ef537f99cf6864040b2fa8968b
Andrew Hundt <ATHundt@gmail.com>
2016-11-10 01:44:34 -0500

    sort call on separate line

--- mapper.py
+++ mapper.py
@@ -20,6 +20,7 @@ for line in sys.stdin:
             for j in xrange(i+1,wordcount):
                 for k in xrange(j+1,wordcount):
                     print 'test3, i:%d j:%d k:%d words[j]:%s words[k]:%s' % (i,j,k,words[j],words[k])
-                    slist = [words[j], words[k]].sort()
+                    slist = [words[j], words[k]]
+                    slist.sort()
                     print slist
                     print '<%s,%s,%s>\t%s' % ( words[i], slist[0], slist[1], 1)
