df0e706072e3874ba1d42b2be18a52268f82171a
Andrew Hundt <ATHundt@gmail.com>
2016-11-10 18:54:51 -0500

    commands in setup.sh

--- setup.sh
+++ setup.sh
@@ -9,3 +9,49 @@ export JAVA_HOME=/usr/lib/jvm/java-7-oracle/
 hadoop com.sun.tools.javac.Main FoF.java
 jar cf FoF.jar FoF*.class
 hadoop jar ./FoF.jar FoF simple.input simple.output
+
+
+# hadoop fs -mkdir -p /user/parallel
+# parallel@parallel-pr3|~/parallel_programming_hw3 on master
+# hadoop fs -copyFromLocal simple.output /user/parallel/simple.input
+
+## Run Hadooop:
+#  git pull && hadoop com.sun.tools.javac.Main FoF.java && jar cf FoF.jar FoF*.class && hadoop jar ./FoF.jar FoF simple.input simple.output
+
+# hadoop fs -put /user/parallel/simple.output
+# hadoop fs -rm -r /user/parallel/simple.output
+
+# mkdir
+# hadoop fs -mkdir -p /user/parallel/simple.output
+
+
+
+# other command line commands:
+
+# cat simple.input/* | python fof.mapper.py  | sort | python fof.reducer.py
+
+
+# hadoop/sbin/start-all.sh
+
+
+# hadoop fs -put simple.input /
+
+# hadoop fs -rm -r /simple.output
+
+
+# hadoop jar /home/parallel/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.7.3.jar -mapper 'python fof.mapper.py' -reducer 'python fof.reducer.py' -input /simple.input -output simple.output
+
+
+# hadoop fs -get simple.output ~/parallel
+
+
+
+
+
+
+# ———————————————————
+
+
+
+
+# hadoop fs -copyFromLocal simple.output /
