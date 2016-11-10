7f32a7126048b8d3b839a30f359a699f7b64ccbc
Andrew Hundt <ATHundt@gmail.com>
2016-11-10 14:14:55 -0500

    hadoop jar command added

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 
 export HADOOP_CLASSPATH=$JAVA_HOME/lib/tools.jar
 
+hadoop jar hadoop/share/hadoop/tools/lib/hadoop-streaming-2.7.3.jar -mapper ./fof.mapper.pl -reducer fof.reducer.pl -input simple.input -output simple.output
+
+
 hadoop com.sun.tools.javac.Main FoF.java
 jar cf FoF.jar FoF*.class
 hadoop jar ./FoF.jar FoF simple.input simple.output
