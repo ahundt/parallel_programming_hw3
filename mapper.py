c0c245b2b79dbba9f55a528d30a736ec015e0db0
Andrew Hundt <ATHundt@gmail.com>
2016-11-10 01:32:06 -0500

    more test printing

--- mapper.py
+++ mapper.py
@@ -5,18 +5,21 @@ import sys
 
 # input comes from STDIN (standard input)
 for line in sys.stdin:
+    print "test1"
     # remove leading and trailing whitespace
     line = line.strip()
     # split the line into words
     words = line.split()
     wordcount = len(words)
 
+    print "test2"
     print words
     # increase counters
     if wordcount >= 3:
         for i in xrange(wordcount):
             for j in xrange(i+1,wordcount):
                 for k in xrange(j+1,wordcount):
+                    print "test3"
                     slist = [words[j], words[k]].sort()
                     print slist
                     print '<%s,%s,%s>\t%s' % ( words[i], slist[0], slist[1], 1)
